d2c68d3b0fe09d4bee887887cd2f39205e0bb535
YoungProducer <37039996+YoungProducer@users.noreply.github.com>
2019-09-23 16:36:27 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { signUp, signIn } from "./middleware"
 import { SignUp, SignIn } from "./components/Auth";
+import axios from "axios"
 
 export class App extends React.Component {
+    componentDidMount() {
+        axios.patch('http://lvh.me:3000/users/changeid', {
+            id: "jopa",
+            email: "sashabezrukovownmail@gmail.com"
+        }, {
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IiIsIm5hbWUiOiJTYXNoYSBCZXpydWtvdiIsImlhdCI6MTU2OTIzNzQzMiwiZXhwIjoxNTY5NDUzNDMyfQ.NOEImmpjnVNJ33WKaaFLPWhsisaIWORLPF4lqtzw5wE"
+            }
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error.response));
+    }
+
+
     render() {
         return(
             <div>
